921684a422f76528de0516ce4ed58452475d5ab0
Bulat Musin <66408714+Fostonger@users.noreply.github.com>
2024-02-19 21:21:31 +0300

    fix feign in auth

--- auth-service/src/main/java/com/startit/authservice/service/UserServiceClient.java
+++ auth-service/src/main/java/com/startit/authservice/service/UserServiceClient.java
@@ -4,12 +4,15 @@ import com.startit.authservice.transfer.User;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.Optional;
 
 @FeignClient(name = "user-service")
 public interface UserServiceClient {
     @GetMapping(value = "/api/v1/user/username/{username}")
-    User findByUsername(@PathVariable String username);
-    @GetMapping(value = "/api/v1/user")
+    Optional<User> findByUsername(@PathVariable String username);
+    @PostMapping(value = "/api/v1/user/save")
     Long save(@RequestBody User user);
 }
