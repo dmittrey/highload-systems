fb2f3205a9990fb1d7d81b5c4df5e14b98706993
d.zubakhin <dmitryzubakhin@mail.ru>
2024-02-24 23:40:52 +0300

    Config-server: Clear all mcs configs
    Moved to external git repo

--- gateway-service/src/main/java/com/startit/gatewayservice/jwt/JwtService.java
+++ gateway-service/src/main/java/com/startit/gatewayservice/jwt/JwtService.java
@@ -6,6 +6,7 @@ import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
@@ -15,6 +16,7 @@ import java.util.Map;
 import java.util.function.Function;
 
 @Service
+@RefreshScope
 public class JwtService {
 
     @Value("${application.security.jwt.secret-key}")
